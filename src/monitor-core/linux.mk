09fea9b32a3509972df6ed54e8bd61059befbb06
Anoop Rajendra <anoop.rajendra@gmail.com>
2009-04-21 23:42:56 +0000

    Moved the build target from common Makefile to OS specific makefile

--- src/monitor-core/linux.mk
+++ src/monitor-core/linux.mk
@@ -1,3 +1,20 @@
+build:	
+	gunzip -c $(ARCHIVE)-$(VERSION).tar.gz | $(TAR) -xf -
+	( 							\
+		cd patch-files;					\
+		find . -type f | grep -v CVS | cpio -pduv 	\
+			../$(ARCHIVE)-$(VERSION);		\
+		cd ../$(ARCHIVE)-$(VERSION);			\
+		./configure 					\
+			--prefix=$(PKGROOT)			\
+			--with-gmetad 				\
+			--with-libconfuse=/opt/confuse/		\
+			CFLAGS="-I/opt/rocks/include/"		\
+			CPPFLAGS="-I/opt/rocks/include/"	\
+			LDFLAGS="-L/opt/rocks/lib/";		\
+		$(MAKE);					\
+	)
+
 install::
 GMOND_INIT	= gmond/gmond.init
 GMETAD_INIT	= gmetad/gmetad.init
--- src/monitor-core/sunos.mk
+++ src/monitor-core/sunos.mk
@@ -1,3 +1,21 @@
+build:	
+	gunzip -c $(ARCHIVE)-$(VERSION).tar.gz | $(TAR) -xf -
+	( 							\
+		cd patch-files;					\
+		find . -type f | grep -v CVS | cpio -pduv 	\
+			../$(ARCHIVE)-$(VERSION);		\
+		cd ../$(ARCHIVE)-$(VERSION);			\
+		./configure 					\
+			--prefix=$(PKGROOT)			\
+			--with-gmetad 				\
+			--with-libconfuse=/opt/confuse/		\
+			--with-libexpat=/usr/sfw		\
+			CFLAGS="-I/opt/rocks/include/ -I/usr/sfw"\
+			CPPFLAGS="-I/opt/rocks/include/ -I/usr/sfw"\
+			LDFLAGS="-L/opt/rocks/lib/";		\
+		$(MAKE);					\
+	)
+
 install::
 GMOND_INIT	= gmond/gmond.init.solaris
 GMETAD_INIT	= gmetad/gmetad.init.solaris
