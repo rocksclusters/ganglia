c2ed21de83f0c3ca5ecd6814f0cf3431bf82ecc8
Anoop Rajendra <anoop.rajendra@gmail.com>
2010-11-28 01:03:04 +0000

    Use gcc instead of cc. Also hardcode sysconfdir for this version of ganglia

--- src/monitor-core/sunos.mk
+++ src/monitor-core/sunos.mk
@@ -7,11 +7,13 @@ build:
 		cd ../$(ARCHIVE)-$(VERSION);			\
 		./configure 					\
 			--prefix=$(PKGROOT)			\
+			--sysconfdir=/etc/ganglia		\
 			--without-gmetad 			\
 			--without-librrd			\
 			--with-python=/opt/rocks		\
 			--with-libconfuse=/opt/confuse/		\
 			--with-libexpat=/usr/sfw		\
+			CC=gcc					\
 			CFLAGS="-I/opt/rocks/include/ -I/usr/sfw"\
 			CPPFLAGS="-I/opt/rocks/include/ -I/usr/sfw"\
 			LDFLAGS="-L/opt/rocks/lib/";		\
