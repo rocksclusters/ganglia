d1731cc9e61788192d97999b04d14899f5f6b5eb
Anoop Rajendra <anoop.rajendra@gmail.com>
2011-08-24 18:30:04 +0000

    Ganglia build on Solaris now fixed. pod2man is a new perldoc utility, and only exists in later versions of Perl. So disable perldoc building during ganglia builds on Solaris

--- src/monitor-core/sunos.mk
+++ src/monitor-core/sunos.mk
@@ -4,7 +4,15 @@ build:
 		cd patch-files;					\
 		find . -type f | grep -v CVS | cpio -pduv 	\
 			../$(ARCHIVE)-$(VERSION);		\
-		cd ../$(ARCHIVE)-$(VERSION);			\
+	)
+	for i in `find ganglia-3.2.0 -type f -name Makefile.\*`; do \
+			sed -e "/[\t ]*pod2/s/pod2html/-pod2html/g" \
+			-e "/[\t ]*pod2/s/pod2text/-pod2text/g"     \
+			-e "/[\t ]*pod2/s/pod2man/-pod2man/g" $$i > $${i}.tmp; \
+			mv $${i}.tmp $$i; \
+		done
+		(						\
+		cd $(ARCHIVE)-$(VERSION);			\
 		./configure 					\
 			--prefix=$(PKGROOT)			\
 			--sysconfdir=/etc/ganglia		\
@@ -13,7 +21,6 @@ build:
 			--with-python=/opt/rocks		\
 			--with-libconfuse=/opt/confuse/		\
 			--with-libexpat=/usr/sfw		\
-			CC=gcc					\
 			CFLAGS="-I/opt/rocks/include/ -I/usr/sfw"\
 			CPPFLAGS="-I/opt/rocks/include/ -I/usr/sfw"\
 			LDFLAGS="-L/opt/rocks/lib/";		\
