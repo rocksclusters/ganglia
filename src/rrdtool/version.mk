a23ca6e594f30e3aff701eccf811fe0675dfb90f
Philip Papadopoulos <ppapadopoulos@ucsd.edu>
2017-08-29 10:26:03 -0700

    update version of rrdtool. Set RPM.FILES

--- src/rrdtool/version.mk
+++ src/rrdtool/version.mk
@@ -1,4 +1,13 @@
 TNAME = rrdtool
 NAME = foundation-rrdtool
-VERSION = 1.4.8
+VERSION = 1.7.0
 RELEASE = 1
+RPM.FILES = \
+/opt/rocks/bin/*\n\
+/opt/rocks/include/*\n\
+/opt/rocks/lib/*\n\
+/opt/rocks/share/doc/*\n\
+/opt/rocks/share/locale/hu/LC_MESSAGES/*\n\
+/opt/rocks/share/man/man1/*\n\
+/opt/rocks/share/man/man3/*\n\
+/opt/rocks/share/rrdtool
