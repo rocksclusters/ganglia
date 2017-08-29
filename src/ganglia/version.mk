b12cd45f22ca6e00fbf89ec81dd596216ef0558c
Philip Papadopoulos <ppapadopoulos@ucsd.edu>
2017-08-29 10:39:52 -0700

    Update RPM.FILES

--- src/ganglia/version.mk
+++ src/ganglia/version.mk
@@ -1,2 +1,9 @@
 NAME = rocks-ganglia
 RELEASE = 3
+RPM.FILES = \
+/etc/cron.weekly/ganglia-save-rrds.cron\n\
+/etc/profile.d/*\n\
+/etc/rc.d/init.d/*\n\
+/opt/ganglia/etc/*\n\
+/opt/ganglia/lib64/ganglia/*\n\
+/opt/rocks/var/plugins/insertethers/*
